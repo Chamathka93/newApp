f71c120ad5b36b056bcffe2714738dcfcb8827b3
Chamathka93 <chamathkatharushi401@gmail.com>
2025-05-09 20:21:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,69 +1,32 @@
-const form = document.getElementById('item-form');
-const nameInput = document.getElementById('itemName');
-const categoryInput = document.getElementById('itemCategory');
-const priceInput = document.getElementById('itemprice');
-const list = document.getElementById('itemList');
-const totalCosts = document.getElementById('totalCost');
-const sortBtn = document.getElementById('sortPrice');
+let list = document.querySelectorAll('.list .item');
+list.forEach(item => {
+    item.addEventListener('click', function(event){
+        if(event.target.classList.contains('add')){
+            var itemNew = item.cloneNode(true);
+            let checkIsset = false;
 
-let items = JSON.parse(localStorage.getItem('shoppingList')) || [];
-
-function saveItems(){
-    localStorage.setItem('shoppingList', JSON.stringify(items));
+            let listCart = document.querySelectorAll('.cart .item');
+            listCart.forEach(cart =>{
+                if(cart.getAttribute('data-key') == itemNew.getAtribute('data-key')){
+                    checkIsset = true;
+                    cart.classList.add('danger');
+                    setTimeout(function(){
+                        cart.classList.remove('danger');
+                    },1000)
+                }
+            })
+            if(checkIsset == false){
+                document.querySelector('.listCart').appendChild(itemName);
+            }
+        }
+    })
+})
+function remove($key){
+    let listCart = document.querySelectorAll('.cart .item');
+    listCart.forEach(item => {
+        if(item.getAttribute('data-key') == $key){
+            item.remove();
+            return;
+        }
+    })
 }
-
-function renderItems(){
-    list.innerHTML = '';
-    let total = 0;
-
-    items.forEach((items, index) => {
-        const li = document.createElement('li');
-        li.className = items.bought? 'bought': '';
-        li. innerHTML = `
-        <div>
-        <strong>${items.name}</strong>
-        (${item.category}) -$$ {item.price.toFixed(2)}
-        </div>
-        <div>
-        <button onclick="toggleBought(${index})"><i class="fa-solid fa-thumbs-up"></i></button>
-        <button onclick="removeItem(${index})"><i class="fa-solid fa-xmark"></i></button>
-        </div>
-
-        `;
-        list.appendChild(li);
-        if(!item.bought)total+=item.price;
-    });
-
-    totalCosts.textContent = `Total: $${total.toFixed(2)}`;
-
-}
-
-
-
-function add(){
-      const addName = nameInput.value.trim();
-      const addCategory = categoryInput.value.trim();
-      const addPrice = priceInput.value.trim();
-
-      if()
-}
-
-function toggleBought(index){
-    items[index].bought = !items[index].bought;
-    saveItems();
-    renderItems();
-}
-
-function removeItem(index){
-    items.splice(index,1);
-    saveItems();
-    renderItems();
-}
-
-sortBtn.addEventListener('click', () =>{
-    items.sort((a,b) => a.price - b.price);
-    saveItems();
-    renderItems();
-});
-
-renderItems();
